250ed04555349853f606ea9cef7df4fe11f4eaed
Shwet Prakash <krish.shwetprakash08@gmail.com>
2024-01-21 00:56:38 +0530

    Frontend Navbar completed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,53 +8,55 @@ const Header = () => {
   const path = useLocation().pathname;
   return (
     <Navbar className="border-b-2">
-      <Link
-        to="/"
-        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
-      >
-        {/* todo: Customize gradient */}
-        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-          Shiro's
-        </span>
-        StoryBook
-      </Link>
-
-      <form>
-        <TextInput
-          type="text"
-          placeholder="Search"
-          className="hidden lg:inline"
-          rightIcon={AiOutlineSearch}
-        />
-      </form>
-      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
-        <AiOutlineSearch />
-      </Button>
-
-      <div className="flex gap-2 md:order-2">
-        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
-          <FaMoon />
-        </Button>
-        <Link to="/sign-in">
-          <Button gradientDuoTone={"purpleToBlue"}>Sign In</Button>
+        <Link
+          to="/"
+          className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+        >
+          {/* todo: Customize gradient */}
+          <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+            Shiro's
+          </span>
+          StoryBook
         </Link>
 
-        {/* Adding Toggle Button for Navbar :: Order 1 */}
-        <Navbar.Toggle />
-      </div>
+        <form>
+          <TextInput
+            type="text"
+            placeholder="Search"
+            className="hidden lg:inline"
+            rightIcon={AiOutlineSearch}
+          />
+        </form>
+        <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+          <AiOutlineSearch />
+        </Button>
 
-      {/* todo: Customize active color */}
-      <Navbar.Collapse>
-        <Navbar.Link active={path === "/"} as={"div"}>
-          <Link to="/">Home</Link>
-        </Navbar.Link>
-        <Navbar.Link active={path === "/about"} as={"div"}>
-          <Link to="/about">About</Link>
-        </Navbar.Link>
-        <Navbar.Link active={path === "/projects"} as={"div"}>
-          <Link to="/projects">Projects</Link>
-        </Navbar.Link>
-      </Navbar.Collapse>
+        <div className="flex gap-2 md:order-2">
+          <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
+            <FaMoon />
+          </Button>
+          <Link to="/sign-in">
+            <Button gradientDuoTone={"purpleToBlue"} outline>
+              Sign In
+            </Button>
+          </Link>
+
+          {/* Adding Toggle Button for Navbar :: Order 1 */}
+          <Navbar.Toggle />
+        </div>
+
+        {/* todo: Customize active color */}
+        <Navbar.Collapse>
+          <Navbar.Link active={path === "/"} as={"div"}>
+            <Link to="/">Home</Link>
+          </Navbar.Link>
+          <Navbar.Link active={path === "/about"} as={"div"}>
+            <Link to="/about">About</Link>
+          </Navbar.Link>
+          <Navbar.Link active={path === "/projects"} as={"div"}>
+            <Link to="/projects">Projects</Link>
+          </Navbar.Link>
+        </Navbar.Collapse>
     </Navbar>
   );
 };
