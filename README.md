da0404f02896c9be088c6b2f3b3edf8dcb146fdf
Shwet Prakash <krish.shwetprakash08@gmail.com>
2024-01-20 20:42:13 +0530

    Pages and Routes created

---
+++ README.md
@@ -0,0 +1,17 @@
+# StoryBook : A Blog Post Site with Dashboard
+
+
+## Tech Stack
+1. React
+2. Tailwind CSS
+3. MongoDB
+
+## Daily Progress
+> 20th January 2024
+- Set basic template
+  - React installed with Vite
+  - Tailwind configured
+  
+- Set Pages and Routes
+  - Basic templates pages : About, SignIn, SignUp, Projects, Dashboard, Home
+  - Installed `react-router-dom` and created Routes
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Dashboard from "./pages/Dashboard";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 
 const App = () => {
-  return <div className="text-3xl text-red-400">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+
+      </Routes>
+    </BrowserRouter>
+  )
 };
 
 export default App;
